c0a51bd5a7f06aa26e953afabfdaf55e3558ed5e
Marcin Jasion <marcin.jasion@gmail.com>
2015-05-29 14:50:56 +0200

    webclient production

---
+++ codepot-webclient-production.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd /home/codepot/codepot-webclient-production
+
+git fetch -f
+git reset --hard origin/master
+docker build --tag codepot-webclient-production .
+docker rm -f codepot-webclient-production
+docker run -v `pwd`/dist:/app/dist --name codepot-webclient-production -e API_HOST="https://backend.codepot.pl/" codepot-webclient-production
