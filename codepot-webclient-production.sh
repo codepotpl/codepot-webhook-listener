3c2381d8640bfb1c95f9470c02287d132b632684
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-06-22 21:36:15 +0200

    Webclient production settings.

--- codepot-webclient-production.sh
+++ codepot-webclient-production.sh
@@ -2,8 +2,8 @@
 
 cd /home/codepot/codepot-webclient-production
 
-git fetch -f
-git reset --hard origin/master
-docker build --tag codepot-webclient-production .
-docker rm -f codepot-webclient-production
-docker run -v `pwd`/dist:/app/dist --name codepot-webclient-production -e API_HOST="https://backend.codepot.pl/" codepot-webclient-production
+git fetch -f;
+git reset --hard origin/master;
+docker build --tag codepot-webclient-production .;
+docker rm -f codepot-webclient-production;
+docker run -v `pwd`/dist:/app/dist --name codepot-webclient-production -e API_HOST="https://backend.codepot.pl/" -e BASE_URL="https://registration.codepot.pl/" codepot-webclient-production;
