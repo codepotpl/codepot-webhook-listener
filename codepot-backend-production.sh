2b4cb3267360b892f4a3f725bcfa81aa021de7f2
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-20 01:15:56 +0200

    refactor

--- codepot-backend-production.sh
+++ codepot-backend-production.sh
@@ -12,9 +12,7 @@ docker-compose up -d;
 sh /home/codepot/codepot-nginx-production/run.sh
 bash ~/codepot-metrics/run.sh
 
-refresh_elasticsearch() {
-    sleep 30
-    django_container=`docker ps | grep django_ | sed 's/ \{2,\}/,/g' | cut -d ',' -f 7 | head -n 1`
-    docker exec -it $django_container python manage.py rebuild_index -v2 --remove --noinput
-}
-refresh_elasticsearch &
+echo "Waiting for rebuild index"
+sleep 20
+django_container=`docker ps | grep django_ | sed 's/ \{2,\}/,/g' | cut -d ',' -f 7 | head -n 1`
+docker exec -it $django_container python manage.py rebuild_index -v2 --remove --noinput
